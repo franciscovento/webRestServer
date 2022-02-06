0e539a3ba2bb937779e1efb9b634da7b6a0f787b
fran <francisco.vento@outlook.com>
2022-02-06 11:39:38 -0300

    port

--- models/server.js
+++ models/server.js
@@ -7,7 +7,7 @@ class  Server {
 
   constructor(){
     this.app = express();
-    this.port = process.env.PORT || 80,
+    this.port = process.env.PORT,
     this.usuariosPath = '/api/usuarios';
   
     // Middlewares
